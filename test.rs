422626f5c6db63b123b92cb48d10829bfdd0f4c1
Robert Oleynik <robert-oleynik@t-online.de>
2022-06-17 16:25:16 +0200

    Add rust test file

---
+++ test.rs
@@ -0,0 +1,19 @@
+//! Test
+
+use std::io::stdio;
+
+/// Test
+///
+///
+fn main() {
+    return;
+}
+
+pub struct Test;
+
+impl Test {
+    /// Test
+    fn new() -> Self {
+        Self {}
+    }
+}
