81fd54d645b6d7387f4de82d26953c8ac6e2d926
Robert Oleynik <robert-oleynik@t-online.de>
2022-06-17 15:54:02 +0200

    Add setup function.+

--- lua/git-blame.lua
+++ lua/git-blame.lua
@@ -1,6 +1,21 @@
 local M = {}
 
-function M.setup()
+function M.setup(options)
+	if vim.g.git_blame == nil then
+		vim.g.git_blame = {}
+	end
+
+	if options == nil then
+		options = {}
+	end
+
+	local defaults = {
+		git_blame = {
+			debug = false
+		}
+	}
+
+	vim.g.git_blame = vim.tbl_deep_extend('keep', options, vim.g.git_blame, defaults)
 end
 
 return M
